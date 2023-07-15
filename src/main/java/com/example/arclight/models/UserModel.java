c3fcf851f741cd09dc49ffe3f9aca1d1c0eedfd1
iveriquenkayilu <iveriquenkayilu@gmail.com>
2023-07-15 14:38:09 -0400

    Added Secondary Language to User Profile
    Renamed birthDay to birthDate

--- src/main/java/com/example/arclight/models/UserModel.java
+++ src/main/java/com/example/arclight/models/UserModel.java
@@ -1,5 +1,7 @@
 package com.example.arclight.models;
 
+import com.example.arclight.entities.User;
+import com.example.arclight.entities.datatypes.LanguageOption;
 import com.example.arclight.shared.helpers.StringHelper;
 
 import java.time.LocalDate;
@@ -9,21 +11,32 @@ public class UserModel {
     public Long id;
     public  String firstName;
     public  String lastName;
-    public LocalDate birthDay;
+    public LocalDate birthDate;
     public  String email;
 
     private String image;
 
     public Integer age;
+    private LanguageOption  secondaryLanguage;
 
     public UserModel(Long id, String name, String surname, LocalDate birthDay, String email, Integer age, Long imageId) {
         this.id = id;
         this.firstName= name;
         this.lastName= surname;
         this.email=email;
-        this.birthDay= birthDay;
+        this.birthDate = birthDay;
         this.age = age==null? getAge(birthDay): age;
         this.image= StringHelper.GetFileUrl(imageId);
+    }
+    public  UserModel(User user)
+    {   this.id = user.Id;
+        this.firstName= user.firstName;
+        this.lastName= user.lastName;
+        this.email=user.getEmail();
+        this.birthDate = user.birthDate;
+        this.age = age==null? getAge(birthDate): age;
+        this.image= StringHelper.GetFileUrl(user.getImageId());
+        this.secondaryLanguage=user.getSecondaryLanguage();
     }
 
     private  Integer getAge(LocalDate birthDay){
@@ -40,5 +53,13 @@ public class UserModel {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public LanguageOption getSecondaryLanguage() {
+        return secondaryLanguage;
+    }
+
+    public void setSecondaryLanguage(LanguageOption secondaryLanguage) {
+        this.secondaryLanguage = secondaryLanguage;
     }
 }
